e5f0f5be7b89db61d73e7a834b9cace444594fb4
Simon Byrne <simonbyrne@gmail.com>
2017-01-10 15:28:41 +0000

    Files from http://jaeckel.16mb.com/LetsBeRational.7z

---
+++ version.h
@@ -0,0 +1,4 @@
+/* This is file was generated automatically at 2016-03-25 at 08:52:00. Please do not edit. */
+#define REVISION         1035
+#define VERSION   "1.0.0.1035"
+#define COPYRIGHT "© 2013-2016 Peter Jäckel"
